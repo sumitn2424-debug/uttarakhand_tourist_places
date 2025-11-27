7d607f93ca2e38671fe47eb8d921a89d090f31c5
sumitn2424-debug <sumitn2424@gmail.com>
2025-11-28 00:35:18 +0530

    tourist places of uttarakhand

---
+++ data.js
@@ -0,0 +1,47 @@
+const places = {
+  "Kedarnath": {
+    minister: "Pushkar Singh Dhami",
+    info: "Kedarnath is a holy Hindu town dedicated to Lord Shiva, located in the Himalayas at 3,583 m above sea level. One of the most important pilgrimage sites in India.",
+    image: "images/kedarnath.jpg",
+    shortInfo: "Holy town dedicated to Lord Shiva.",
+    location: "Uttarakhand, India",
+    famousFor: "Kedarnath Temple, Himalayan views",
+    bestTime: "May-June & Sep-Oct",
+    altitude: "3,583 m",
+    nearby: "Gaurikund, Vasuki Tal, Chorabari Glacier"
+  },
+  "Badrinath": {
+    minister: "Pushkar Singh Dhami",
+    info: "Badrinath Temple is dedicated to Lord Vishnu and is part of Char Dham pilgrimage circuit.",
+    image: "images/badrinath.jpg",
+    shortInfo: "Temple of Lord Vishnu.",
+    location: "Chamoli, Uttarakhand",
+    famousFor: "Badrinath Temple, River Alaknanda",
+    bestTime: "May-Oct",
+    altitude: "3,133 m",
+    nearby: "Mana Village, Tapt Kund"
+  },
+  "Yamunotri": {
+    minister: "Pushkar Singh Dhami",
+    info: "Yamunotri Temple is dedicated to Goddess Yamuna, located at the origin of Yamuna river.",
+    image: "images/yamunotri.jpg",
+    shortInfo: "Temple of Goddess Yamuna.",
+    location: "Uttarkashi, Uttarakhand",
+    famousFor: "Yamunotri Temple, Hot Springs",
+    bestTime: "May-June & Sep-Oct",
+    altitude: "3,293 m",
+    nearby: "Divya Shila, Janki Chatti"
+  },
+  "Gangotri": {
+    minister: "Pushkar Singh Dhami",
+    info: "Gangotri is the origin point of the holy river Ganga, surrounded by Himalayan peaks.",
+    image: "images/gangotri.jpg",
+    shortInfo: "Origin of River Ganga.",
+    location: "Uttarkashi, Uttarakhand",
+    famousFor: "Gangotri Glacier, Ganga river source",
+    bestTime: "May-June & Sep-Oct",
+    altitude: "3,415 m",
+    nearby: "Bhojwasa, Chirbasa"
+  }
+  // You can add more places here
+};
